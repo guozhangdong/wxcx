cdc782a430aa3feb9af9eb2d3654284c45e441af
guozhangdong <guozhangdong01@gmail.com>
2025-09-05 12:40:12 +0800

    Delete backend directory

--- backend/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const Router = require('koa-router');
-const authController = require('../controllers/authController');
-
-const router = new Router();
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-
-module.exports = router;
--- backend/utils/jwt.js
+++
@@ -1,5 +0,0 @@
-const jwt = require('jsonwebtoken');
-const secret = process.env.JWT_SECRET || 'secret';
-
-exports.sign = payload => jwt.sign(payload, secret, { expiresIn: '7d' });
-exports.verify = token => jwt.verify(token, secret);
